af5e669573f3afe4bc900d777633d1fca914c56e
OriBenZaken <orinetanel1@gmail.com>
2018-05-30 15:02:42 +0300

    done.

--- ex4.py
+++ ex4.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerLine2D
 
 # Hyper-parameters
-BATCH_SIZE = 50
+BATCH_SIZE = 40
 IMAGESIZE = 28 * 28
 LEARN_RATE = 0.01
 EPOCHS = 10
@@ -18,9 +18,20 @@ FIRST_HIDDEN_LAYER_SIZE = 100
 SECOND_HIDDEN_LAYER_SIZE = 50
 NUMBER_OF_CLASSES = 10
 
-class FashionMnistModelTrainer(object):
+class ModelTrainer(object):
+    """
+    Trains a given model on a given set of data for train and validation.
+    """
 
     def __init__(self, train_loader, validation_loader, test_loader, model, optimizer):
+        """
+        initializes the ModelTrainer.
+        :param train_loader: training set
+        :param validation_loader: validation set
+        :param test_loader: test set
+        :param model: neural network model
+        :param optimizer: optimizer
+        """
         self.train_loader = train_loader
         self.validation_loader = validation_loader
         self.test_loader = test_loader
@@ -28,17 +39,28 @@ class FashionMnistModelTrainer(object):
         self.optimizer = optimizer
 
     def run(self):
+        """
+        calls train and validation methods as the number of epochs.
+        calls to method that draw the results graph (avg loss per epoch)
+        finally, model is passing on the test set.
+        :return: None
+        """
         avg_train_loss_per_epoch_dict = {}
         avg_validation_loss_per_epoch_dict = {}
         for epoch in range(1, EPOCHS + 1):
             self.train(epoch, avg_train_loss_per_epoch_dict)
             self.validation(epoch, avg_validation_loss_per_epoch_dict)
-            plotTrainAndValidationGraphs(avg_train_loss_per_epoch_dict, avg_validation_loss_per_epoch_dict)
-
-
+        plotTrainAndValidationGraphs(avg_train_loss_per_epoch_dict, avg_validation_loss_per_epoch_dict)
         self.test()
 
     def train(self, epoch, avg_train_loss_per_epoch_dict):
+        """
+        go through all examples on the validation set, calculates perdiction, loss
+        , accuracy, and updating the model parameters.
+        :param epoch: number of epochs
+        :param avg_train_loss_per_epoch_dict: avg loss per epoch dictionary
+        :return: None
+        """
         self.model.train()
         train_loss = 0
         correct = 0
@@ -62,6 +84,13 @@ class FashionMnistModelTrainer(object):
                                                                                             100. * correct / (len(self.train_loader) * BATCH_SIZE)))
 
     def validation(self, epoch_num, avg_validation_loss_per_epoch_dict):
+        """
+        go through all examples on the validation set, calculates perdiction, loss
+        and accuracy
+        :param epoch: number of epochs
+        :param avg_train_loss_per_epoch_dict: avg loss per epoch dictionary
+        :return: None
+        """
         self.model.eval()
         validation_loss = 0
         correct = 0
@@ -79,6 +108,10 @@ class FashionMnistModelTrainer(object):
             100. * correct / len(self.validation_loader)))
 
     def test(self):
+        """
+        writes all the model predictions on the test set to test.pred file.
+        :return:  None
+        """
         self.model.eval()
         test_loss = 0
         correct = 0
@@ -86,7 +119,9 @@ class FashionMnistModelTrainer(object):
         for data, target in self.test_loader:
             output = self.model(data)
             test_loss += F.nll_loss(output, target, size_average=False).item()
+            # get the predicted class out of output tensor
             pred = output.data.max(1, keepdim=True)[1]
+            # add current prediction to predictions list
             pred_string_list.append(str(pred.item()))
             correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
 
@@ -95,13 +130,23 @@ class FashionMnistModelTrainer(object):
             test_loss, correct, len(self.test_loader),
             100. * correct / len(self.test_loader)))
 
+        # writes the prediction to test.pred file
         with open("test.pred", 'w') as test_pred_file:
             test_pred_file.write('\n'.join(pred_string_list))
         pass
 
 
 class FirstNet(nn.Module):
+    """
+    First model version.
+    two hidden layers.
+    activation function between the layers: Relu.
+    """
     def __init__(self, image_size):
+        """
+        defines FirstNet
+        :param image_size:
+        """
         super(FirstNet, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, FIRST_HIDDEN_LAYER_SIZE)
@@ -109,6 +154,11 @@ class FirstNet(nn.Module):
         self.fc2 = nn.Linear(SECOND_HIDDEN_LAYER_SIZE, NUMBER_OF_CLASSES)
 
     def forward(self, x):
+        """
+        forward pass
+        :param x: input
+        :return: prediction
+        """
         x = x.view(-1, self.image_size)
         x = F.relu(self.fc0(x))
         x = F.relu(self.fc1(x))
@@ -117,6 +167,12 @@ class FirstNet(nn.Module):
 
 
 class SecondNet(nn.Module):
+    """
+    Second model version.
+    two hidden layers.
+    activation function between the layers: Relu.
+    dropout between the layers.
+    """
     def __init__(self, image_size):
         super(SecondNet, self).__init__()
         self.image_size = image_size
@@ -125,6 +181,11 @@ class SecondNet(nn.Module):
         self.fc2 = nn.Linear(SECOND_HIDDEN_LAYER_SIZE, NUMBER_OF_CLASSES)
 
     def forward(self, x):
+        """
+        forward pass
+        :param x: input
+        :return: prediction
+        """
         x = x.view(-1, self.image_size)
         x = F.relu(self.fc0(x))
         x = F.dropout(x, training=self.training)
@@ -135,6 +196,12 @@ class SecondNet(nn.Module):
 
 
 class ThirdNet(nn.Module):
+    """
+    First model version.
+    two hidden layers.
+    activation function betweeb the layers: Relu.
+    using batch normalization.
+    """
     def __init__(self, image_size):
         super(ThirdNet, self).__init__()
         self.image_size = image_size
@@ -145,6 +212,11 @@ class ThirdNet(nn.Module):
         self.bn2 = nn.BatchNorm1d(SECOND_HIDDEN_LAYER_SIZE)
 
     def forward(self, x):
+        """
+        forward pass
+        :param x: input
+        :return: prediction
+        """
         x = x.view(-1, self.image_size)
         x = self.bn1(F.relu(self.fc0(x)))
         x = self.bn2(F.relu(self.fc1(x)))
@@ -153,6 +225,14 @@ class ThirdNet(nn.Module):
 
 
 def plotTrainAndValidationGraphs(avg_train_loss_per_epoch_dict, avg_validation_loss_per_epoch_dict):
+    """
+    plot two graphs:
+    1. avg loss per epoch on train set
+    2. avg loss per epoch on validation set
+    :param avg_train_loss_per_epoch_dict: avg train loss per epoch dictionary
+    :param avg_validation_loss_per_epoch_dict: avg validation loss per epoch dictionary
+    :return: None
+    """
     line1, = plt.plot(avg_train_loss_per_epoch_dict.keys(), avg_train_loss_per_epoch_dict.values(), "orange",
                       label='Train average loss')
     line2, = plt.plot(avg_validation_loss_per_epoch_dict.keys(), avg_validation_loss_per_epoch_dict.values(), "purple",
@@ -173,7 +253,7 @@ def main():
                                          train=False,
                                          transform=transforms.ToTensor())
 
-    # spliting to training set and validation set
+    # splitting to training set and validation set
 
     ## We need to further split our training dataset into training and validation sets.
 
@@ -215,7 +295,7 @@ def main():
     model = ThirdNet(image_size=IMAGESIZE)
     optimizer = optim.Adagrad(model.parameters(), lr=LEARN_RATE)
 
-    trainer = FashionMnistModelTrainer(train_loader, validation_loader, test_loader, model, optimizer)
+    trainer = ModelTrainer(train_loader, validation_loader, test_loader, model, optimizer)
     trainer.run()
 
 if __name__ == "__main__":
